e3815f6cd187fea7cc3a0e124a3e4b269dd39352
Vasilisa <vasilisavygovskaya1109@gmail.com>
2024-10-06 13:27:53 +0300

    login&script changes

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 // function validateEmail(email) {
-//     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//     const emailPattern =  /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/g ;
 //     return emailPattern.test(email);
 // }
 
+function validatePassword2() {
+    const passwordField = document.querySelector('.password'); 
+    const password = passwordField.value; 
+    const passwordPattern = /^(?=.*\d)(?=.*[A-Z])[A-Za-z\d]{8,}$/;
+
+    const errorLog = document.getElementById("errorMessage"); 
+
+    if (!passwordPattern.test(password)) {
+        errorLog.innerHTML = "Пароль должен содержать минимум 8 символов, включая 1 заглавную букву и 1 цифру.";
+        return false; 
+    }
+
+    errorLog.innerHTML = ""; 
+    alert('Данные отправлены'); 
+    return true; 
+}
 
 function validatePassword() {
     const passwordField = document.getElementsByClassName('password')[0]; 
     const password = passwordField.value; 
-    const passwordPattern = /^(?=.*\d)(?=.*[A-Z])[A-Za-z\d]{8,}$/; 
+    const passwordPattern = /^(?=.*\d)(?=.*[A-Z])[A-Za-z\d]{8,}$/;
     
-    const passwordRep = document.getElementById('pass2'); 
-    const passwordRepeat = passwordRep.value; 
+    const passwordRep = document.getElementById('pass2');
+    const passwordRepeat = passwordRep.value;
 
     const error = document.getElementById("error-message"); 
-    const error2 = document.getElementById("error2");
 
-    
     if (!passwordPattern.test(password)) {
         error.innerHTML = "Пароль должен содержать минимум 8 символов, включая 1 заглавную букву и 1 цифру.";
         return false; 
     }
+
     if (password !== passwordRepeat) {
-        error2.innerHTML = "Пароли не совпадают"; 
+        error.innerHTML = "Пароли не совпадают"; 
         return false; 
     }
 
-    error.innerHTML = "";
-    error2.innerHTML = ""; 
+    error.innerHTML = ""; 
     alert('Данные отправлены'); 
     return true; 
 }
